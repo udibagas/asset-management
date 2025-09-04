9433000460d45c21f8ee64b7305bc976f986e789
udibagas <udibagas@gmail.com>
2025-09-04 11:14:19 +0700

    update vite

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'], true)
     <title>@yield('title')</title>
 </head>
 
